8cc7c89915981eca4ff0b9f6113762daf1211827
badgersky <szymonek.ptasznik@gmail.com>
2023-07-06 13:18:27 +0200

    create window for an app, create main.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import customtkinter as ctk
+from tkinter import filedialog
+
+
+class App(ctk.CTk):
+
+    def __init__(self):
+        super().__init__()
+        self.geometry('300x300')
+        self.title('PDFmerge-tool')
+
+
+if __name__ == '__main__':
+    pass
