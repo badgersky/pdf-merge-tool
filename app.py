39ddb07e1823b45464e60f0a01b421bffac0ee70
badgersky <szymonek.ptasznik@gmail.com>
2023-07-06 13:37:33 +0200

    changed window geometry, add buttons on main window

--- app.py
+++ app.py
@@ -6,9 +6,21 @@ class App(ctk.CTk):
 
     def __init__(self):
         super().__init__()
-        self.geometry('300x300')
+        self.geometry('300x150')
         self.title('PDFmerge-tool')
 
+        self.rowconfigure((0, 1, 2), weight=1)
+        self.columnconfigure(0, weight=1)
+
+        # buttons on main window
+        self.btn_merge = ctk.CTkButton(self, width=150, height=30, text='MERGE PDF')
+        self.btn_merge.grid(row=0, column=0, padx=10, pady=10)
+
+        self.btn_split = ctk.CTkButton(self, width=150, height=30, text='SPLIT PDF')
+        self.btn_split.grid(row=1, column=0, padx=10, pady=10)
+
+        self.btn_quit = ctk.CTkButton(self, width=150, height=30, text='QUIT')
+        self.btn_quit.grid(row=2, column=0, padx=10, pady=10)
 
 if __name__ == '__main__':
     pass
